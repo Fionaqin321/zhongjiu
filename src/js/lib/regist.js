10c030a6ef14f45a898fa9bf619557d932cde15d
fionaqin <fionaqin321@163.com>
2020-07-04 16:02:35 +0800

    add regist

--- src/js/lib/regist.js
+++ src/js/lib/regist.js
@@ -5,58 +5,84 @@ define(['jquery'], function($) {
                 "regName": /^[A-z]{5,15}/,
                 "cellPhone": /^1[35789]\d{9}$/,
                 "pwd": /^.{6,16}$/
-                    // "email": /^[A-z]\w{5,15}@[A-z0-9-]{2,}\.[A-z]{2,7}\.?[A-z]*$/
             }
             var inputs = $('.form .regTest'); // 需要验证正则的input
             var pass = $('#pwdRepeat'); // 确认密码
-            var sysCode = $('#syscheckCode') // 手机验证码
-                // var protocol = $('.checkbox'); // 用户协议
             var submit = $('.btn-regist'); // 提交按钮
 
-            console.log(inputs);
+            inputs.each((idx, item) => {
+                // 输入的时候进行正则验证
+                $(item).on('input', function() {
+                    if (reg[item.id].test(this.value)) {
+                        // 正则验证通过
+                        item.dataset.pass = true;
+                        $(item).parent().find('.info').removeClass('show');
+                        $(item).parent().find('.success').addClass('show');
+                    } else {
+                        item.dataset.pass = false;
+                        $(item).parent().find('.info').addClass('show');
+                        $(item).parent().find('.success').removeClass('show');
+                    }
+                    // console.log(item.dataset.pass);
+                })
+            })
+
+
+            // 确认密码
+            $('#pwdRepeat').on('input', function() {
+                if (this.value === $('#pwd').val()) {
+                    this.dataset.pass = true;
+                    $(this).parent().find('.info').removeClass('show');
+                    $(this).parent().find('.success').addClass('show');
+                } else {
+                    this.dataset.pass = false;
+                    $(this).parent().find('.info').addClass('show');
+                    $(this).parent().find('.success').removeClass('show');
+                }
+            })
+
+            // 点击按钮获得验证码
+            var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+            $('.getCode').on('click', function() {
+                var str = '';
+                while (str.length < 4) {
+                    var iNum = parseInt(Math.random() * 100)
+                    while (iNum > 36) {
+                        iNum = parseInt(Math.random() * 100)
+                    }
+                    str += arr[iNum];
+                }
+                $('#syscheckCode').val(str.toUpperCase());
+                $('#syscheckCode')[0].dataset.pass = true;
+            });
+
+            // 用户是否勾选了用户协议
+            $('.checkbox').on('click', function() {
+                // console.log($(this).is(':checked'));
+                if ($(this).is(':checked')) {
+                    this.dataset.pass = true;
+                    $('#checkAgreement_info').removeClass('show');
+                } else {
+                    this.dataset.pass = false;
+                    $('#checkAgreement_info').addClass('show');
+                }
+            })
+
+
 
             // 提交按钮
             $('.btn-regist').on('click', function() {
-                console.log($('.checkbox').is(':checked'));
-                // $('.checkbox').is(':checked')
+                check();
             })
 
             function check() {
                 // 用户协议
-                var allPass = document.querySelectorAll('#myform>input[data-pass="true"]');
-                if (allPass.length === 5) {
-                    submit.removeAttribute('disabled');
+                var allPass = document.querySelectorAll('.form input[data-pass="true"]');
+                console.log(allPass);
+                if (allPass.length === 6) {
+                    $('.btn-regist').removeAttr('disabled');
                 }
             }
-
-            // 确认密码
-            pass.oninput = function() {
-                    if (this.val() === $('#pwd').val()) {
-                        // passCheck.innerHTML = '通过验证';
-                        this.dataset.pass = true;
-                    } else {
-                        // passCheck.innerHTML = '两次输入的密码不同，请重新输入';
-                        this.dataset.pass = false;
-                    }
-                    check();
-                }
-                // 验证码
-
-            inputs.each((idx, item) => {
-                item.oninput = function() {
-                    if (reg[item.id].test(this.value)) {
-                        // 验证通过
-                        // $('.blank').addClass('show');
-                        item.dataset.pass = true;
-                    } else {
-                        // 验证未通过;
-                        $('.error').addClass('show');
-                        item.dataset.pass = false;
-                    }
-                    // console.log(item.dataset.pass);
-                    check();
-                }
-            })
         }
     }
 })
